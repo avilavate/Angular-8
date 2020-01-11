d9d80b05429f24e246e8efd9773250d4bb8cb121
A <avilavate@live.in>
2020-01-11 20:50:18 +0530

    integrated Product list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template:'<pm-poducts></pm-poducts>',
+  template:'<div><h4>ACME Product Managment</h4><pm-poducts></pm-poducts></div>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
